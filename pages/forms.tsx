9b1a5a0ed776d174c40190455562fc62f4f2a2ad
kimsungwon4 <skim@uppgo.jp>
2022-04-17 13:21:30 +0900

    carrot-market markup

---
+++ pages/forms.tsx
@@ -0,0 +1,66 @@
+import { FieldErrors, useForm } from "react-hook-form";
+
+// Better validation
+// Better Erros (set, clear, display)
+// Have control over inputs
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
+
+export default function Forms() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<LoginForm>({
+    mode: "onChange",
+  });
+  const onValid = (data: LoginForm) => {
+    console.log("im valid bby");
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
+  console.log(watch("email"));
+
+  return (
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input
+        {...register("username", {
+          required: "Username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
+        })}
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") ? "" : "Gamil is not allowed",
+          },
+        })}
+        type="email"
+        placeholder="Email"
+        className={`${
+          Boolean(errors.email?.message) ? "border-red-500 border-4" : ""
+        }`}
+      />
+      {errors.email?.message}
+      <input
+        {...register("password", { required: "Password is required" })}
+        type="password"
+        placeholder="Password"
+      />
+      <input type="submit" value="Create Account" />
+    </form>
+  );
+}
